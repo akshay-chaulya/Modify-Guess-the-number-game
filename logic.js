1a8895d69838cafbb8ae44f8805fc3a1d72bd25b
Akshay Chaulya <akshaychaulya09@gmail.com>
2023-08-30 02:38:55 +0530

    final commit

--- logic.js
+++ logic.js
@@ -1,114 +1,63 @@
-// all elements here
-const pageTitle = document.title
-const nav = document.querySelector(".nav");
-const main = document.querySelector(".main");
-const textBox = document.getElementById("textBox")
-const input = document.getElementById("input");
-const check = document.querySelector(".check");
-const cancel = document.querySelector(".cancel");
-const gameInstruction = document.querySelector(".gameInstruction")
-const gameContainer = document.querySelector(".gameContainer")
+// all elements
+const againBtn = document.getElementById("againBtn");
+const textArea = document.getElementById("textarea");
+const checkBtn = document.getElementById("checkBtn");
+const text1 = document.querySelector(".t1");
+const score = document.getElementById("score");
+const highScore = document.getElementById("highScore");
+const computerNumberBox = document.querySelector(".computerNumberBox")
 
-// assignment prodess
-let text = `Welcom To ${pageTitle} Game`;
-let score = 0;
 
-// random number
-const rNum = Math.floor(Math.random() * 100)
-
-// for reset main height when nav height change.
-const resetmainHeight = function () {
-    const navHeight = nav.offsetHeight;
-    main.style.height = `calc(100vh - ${navHeight}px)`
-}
-resetmainHeight()
-
-// div nav text typing animation effect
-function textTypingAnimation(text, elem, speed) {
-    let i = 0;
-    function typeText() {
-        if (i < text.length) {
-            elem.innerHTML += text.charAt(i);
-        }
-        i++;
-        setTimeout(typeText, speed)
-    }
-    if (elem.innerText === "") {
-        typeText()
-    }
-}
-textTypingAnimation(text, textBox, 50)
-
-// cancel logic
-cancel.addEventListener("click", () => {
-    cancelLogic()
-});
-function cancelLogic() {
-    input.value = "";
-    gameInstructionHeightRemove('15vh')
-    input.focus()
-}
-
-// for check logic
-check.addEventListener("click", () => {
-    checkLogic()
+// again logic 
+againBtn.addEventListener("click", () => {
+    againBtnLogic()
 })
 
-function checkLogic() {
-    const inputValue = Number(input.value);
-    const SPEED = 30;
-    if (inputValue) {
-        score++;
+function againBtnLogic() {
+    location.reload()
+}
+
+// rendom number logic && score
+const rNum = Math.floor(Math.random() * 20);
+
+// check logics
+checkBtn.addEventListener("click", () => {
+    checkBtnLogic()
+})
+
+function checkBtnLogic() {
+    let inputValue = textArea.value;
+    if (inputValue >= 0) {
+        score.innerHTML -= 1;
         if (inputValue > rNum) {
-            text = `Not Matchh! Your Number ${inputValue} Is Biger Then The Computer Number.`
+            text1.innerHTML = "📈 To Big"
         }
         else if (inputValue < rNum) {
-            text = `Not Match! Your Number ${inputValue} Is Smaller Then The Computer Number.`
+            text1.innerHTML = "📉 To Small"
         }
         else {
-            text = `Your Number ${inputValue} Is Match The Computer Number. And Your Score Is *${score}*`
-            gameInstruction.style.color = "#00008b"
+            text1.innerHTML = "🔥 Match"
+            computerNumberBox.innerHTML = rNum
+            document.body.style.background = "rgba(0, 176, 0, 0.678)"
+            setHighScore(score.innerHTML)
         }
-
-
-        if (gameInstruction !== "") {
-            gameInstruction.innerHTML = '';
-            textTypingAnimation(text, gameInstruction, SPEED)
-        }
-        gameInstructionHeightSet('15vh', text, gameInstruction, SPEED)
     }
     else {
-        text = "Pleas enter a number!";
-        if (gameInstruction !== "") {
-            gameInstruction.innerHTML = '';
-            textTypingAnimation(text, gameInstruction, SPEED)
-        }
-        gameInstructionHeightSet('15vh', text, gameInstruction, SPEED)
+        text1.innerHTML = "💀 Please Enter Valid Number!"
     }
-    input.value = ""
-    input.focus()
+    textArea.value = ""
+    textArea.focus()
 }
 
-// gameInstruction height set logic
-
-function gameInstructionHeightSet(x, text, elem, speed) {
-    gameInstruction.style.display = "flex";
-    setTimeout(() => {
-        gameInstruction.style.height = x
-        gameContainer.style.margin = 0
-    }, 0)
-    setTimeout(() => {
-        textTypingAnimation(text, elem, speed)
-    }, 300)
+function setHighScore(score) {
+    const storeScore = localStorage.getItem("highScore");
+    if (score > storeScore) {
+        localStorage.setItem("highScore", `${score}`)
+    }
 }
 
-function gameInstructionHeightRemove(x = null) {
-    gameInstruction.style.height = 0
-    gameContainer.style.marginTop = x
-    setTimeout(() => {
-        gameInstruction.innerHTML = "";
-    }, 700)
-    setTimeout(() => {
-        gameInstruction.style.display = "none";
-    }, 1000)
-}
+setInterval(() => {
+    highScore.innerHTML = localStorage.getItem("highScore")
+}, 50)
+
+// store high score in local storeg
